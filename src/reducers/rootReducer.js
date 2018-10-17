af9b40880b1f5fd0d073dfcc081a5f1bd63ecabe
f2015537 <divyampatro1997@gmail.com>
2018-10-17 12:16:15 +0530

    Done lesson 42

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,3 +1,4 @@
+
 const initState = {
   posts: [
     {id: '1', title: 'Squirtle Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat'},
@@ -9,6 +10,16 @@ const initState = {
 
 
 const rootReducer = (state = initState, action) => {
+  console.log(action);
+  if(action.type === 'DELETE_POST'){
+   let newPosts = state.posts.filter(post => {
+     return post.id !== action.id
+   });
+   return {
+     ...state,
+     posts: newPosts
+   }
+  }
   return state;
 }
 
