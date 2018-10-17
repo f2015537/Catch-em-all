2f392b16f61da4753043d8e8199820f871e52e9a
f2015537 <divyampatro1997@gmail.com>
2018-10-17 11:55:13 +0530

    Some bug fixes to lesson 41

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Post extends Component {
-
   render() {
-      const post = this.props.post ? (
-          <div className="post">
-              <h4 className="center">{this.props.post.title}</h4>
-              <p>{this.props.post.body}</p>
-          </div>
-      ) : (
-          <div className="center">Loading post...</div>
-      )
+
+    const post = this.props.post ? (
+      <div className="post">
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading post...</div>
+    );
+
     return (
       <div className="container">
-        { post }
+        {post}
       </div>
     )
   }
 }
 
-const mapStatetoProps = (state, ownProps) => {
+const mapStateToProps = (state, ownProps) => {
   let id = ownProps.match.params.post_id;
-  return (
+  return {
     post: state.posts.find(post => post.id === id)
-  )
+  }
 }
 
-export default connect(mapStatetoProps)(Post);
+export default connect(mapStateToProps)(Post)
